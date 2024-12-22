d29aec390b9cdb062c810ec16cc57b0573b39c03
erikjhonatan <marcaloayzae@gmail.com>
2024-12-21 23:10:41 -0500

    Implement routing and authentication context in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,56 @@
 import './App.css'
 
+import { 
+  BrowserRouter as Router, 
+  Route,
+  Routes,
+  Navigate,
+  Outlet
+} from 'react-router'
+
+import { useContext } from 'react';
+
+import { AuthContext, AuthProvider } from './context/AuthProvider'
+
 import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+const ComprobatedLogin = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  if (isAuthenticated) {
+    return <Navigate to="/home" />;
+  }
+  return <Outlet />;
+};
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useContext(AuthContext);
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+  return <Outlet />;
+};
+
 
 function App() {
-
-
   return (
-    <>
-      <Home />
-    </>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route element={<ComprobatedLogin />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
