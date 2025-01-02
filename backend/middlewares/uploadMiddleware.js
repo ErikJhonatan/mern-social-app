80fca005e8cec584e6d49bbd164b57a850852a13
erikjhonatan <marcaloayzae@gmail.com>
2025-01-01 22:31:48 -0500

    Adding Cloudinary Configuration and Upload Middleware with Multer

---
+++ backend/middlewares/uploadMiddleware.js
@@ -0,0 +1,23 @@
+import multer from 'multer';
+
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/users');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.username);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+export default upload;
